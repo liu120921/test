5661d9971cd436a84cb6806bf1a48dc1e1402536
liuwenyan <liuwenyan.rhyolite@snda.com>
2018-01-12 15:23:05 +0800

    delete targetpaht

--- buildpak.bat
+++ buildpak.bat
@@ -1,6 +1,6 @@
 @echo off  
 @set unity="C:\Program Files\Unity\Editor\Unity.exe"  
 echo start...  
-%unity%  -batchmode -quit -nographics -executeMethod Exporter.ExportCom  -logFile E:\unity_test_projects\commondlinetest\commondlinetest\Editor.log -projectPath "E:\unity_test_projects\commondlinetest\commondlinetest"   
+%unity%  -batchmode -quit -nographics -executeMethod Exporter.ExportCom  -logFile Editor.log 
 echo finish!  
 pause 
